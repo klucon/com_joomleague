7f0492517b5c112e76086e2fc197bb7d40b8eb30
klucon <ondrej.klucka.mk@seznam.cz>
2026-08-22 21:36:07 +0200

    Make person club schema update repeatable

--- administrator/components/com_joomleague/sql/updates/postgresql/6.2.0-2026081601.sql
+++ administrator/components/com_joomleague/sql/updates/postgresql/6.2.0-2026081601.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "#__joomleague_person" ADD COLUMN "club_id" BIGINT NULL DEFAULT NULL;
-ALTER TABLE "#__joomleague_person" ADD CONSTRAINT "fk_jl_person_club" FOREIGN KEY ("club_id") REFERENCES "#__joomleague_club" ("id") ON DELETE SET NULL;
-CREATE INDEX "idx_jl_person_club" ON "#__joomleague_person" ("club_id");
+ALTER TABLE "#__joomleague_person" ADD COLUMN "club_id" BIGINT NULL DEFAULT NULL /** CAN FAIL **/;
+ALTER TABLE "#__joomleague_person" ADD CONSTRAINT "fk_jl_person_club" FOREIGN KEY ("club_id") REFERENCES "#__joomleague_club" ("id") ON DELETE SET NULL /** CAN FAIL **/;
+CREATE INDEX IF NOT EXISTS "idx_jl_person_club" ON "#__joomleague_person" ("club_id");
